b84a8de0a5066e7a9a34f3e4aaa8f71f43afef96
Pedro Botica <pebotica@gmail.com>
2024-05-04 19:58:47 +0100

    init gallery app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ujjrahctcfbuydtpemhr.supabase.co',
+      }
+    ]
+  }
+};
 
 export default nextConfig;
+
+//ujjrahctcfbuydtpemhr.supabase.co
